cb2797d15602e2bf03e225e45f8522701dcfd3a8
Sjoerd Kuster <sjoerdkuster@gmail.com>
2023-12-17 21:41:44 +0100

    add day 17

---
+++ bin/day17/two.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+
+void main() {
+  final file = File('data.txt');
+
+  final input = file
+      .readAsLinesSync()
+      .map((e) => e.split('').map((e) => int.parse(e)).toList())
+      .toList();
+
+  final seen = <(int, int, int, int, int)>{};
+  final pq = HeapPriorityQueue<(int, int, int, int, int, int)>(
+      (a, b) => a.$1.compareTo(b.$1));
+
+  pq.add((0, 0, 0, 0, 0, 0));
+
+  while (pq.isNotEmpty) {
+    final (heatLoss, row, col, dirRow, dirCol, sameN) = pq.removeFirst();
+
+    if (row == input.length - 1 && col == input[0].length - 1 && sameN >= 4) {
+      print(heatLoss);
+      break;
+    }
+
+    if (seen.contains((row, col, dirRow, dirCol, sameN))) {
+      continue;
+    }
+
+    seen.add((row, col, dirRow, dirCol, sameN));
+
+    if (sameN < 10 && (dirRow, dirCol) != (0, 0)) {
+      int newRow = row + dirRow;
+      int newCol = col + dirCol;
+      if (0 <= newRow &&
+          newRow < input.length &&
+          0 <= newCol &&
+          newCol < input[0].length) {
+        pq.add((
+          heatLoss + input[newRow][newCol],
+          newRow,
+          newCol,
+          dirRow,
+          dirCol,
+          sameN + 1
+        ));
+      }
+    }
+
+    if (sameN >= 4 || (dirRow, dirCol) == (0, 0)) {
+      for (var direction in [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+      ]) {
+        int newDirRow = direction[0];
+        int newDirCol = direction[1];
+        if ((newDirRow != dirRow || newDirCol != dirCol) &&
+            (newDirRow != -dirRow || newDirCol != -dirCol)) {
+          int newRow = row + newDirRow;
+          int newCol = col + newDirCol;
+          if (0 <= newRow &&
+              newRow < input.length &&
+              0 <= newCol &&
+              newCol < input[0].length) {
+            pq.add((
+              heatLoss + input[newRow][newCol],
+              newRow,
+              newCol,
+              newDirRow,
+              newDirCol,
+              1
+            ));
+          }
+        }
+      }
+    }
+  }
+}
